6a68e3d4c209b61fac8d5e462b1b79f9fbc61af4
ottobaier <obaier@wisc.edu>
2019-05-01 16:45:46 -0500

    added stuff to quitWindow

--- application/QuitWindow.java
+++ application/QuitWindow.java
@@ -3,8 +3,10 @@ package application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class QuitWindow implements Window{
@@ -20,7 +22,8 @@ public class QuitWindow implements Window{
   
   @Override
   public Scene getScene() {
-    HBox root = new HBox(20);
+    VBox root = new VBox(20);
+    HBox options = new HBox(20);
     Button quit = new Button("Quit Without Saving");
     quit.setPrefSize(100, 50);
     quit.setFont(Config.SIZE14);
@@ -31,11 +34,16 @@ public class QuitWindow implements Window{
       }
     });
     
-    SwapScreen saveAndQuit = new SwapScreen("Save and Quit", )
+    SwapScreen saveAndQuit = new SwapScreen("Save and Quit", Main.windows[5], stage);
     
-    root.getChildren().add(quit);
     
-    return root;
+    options.getChildren().add(quit);
+    options.getChildren().add(saveAndQuit);
+    Label sure = new Label("Are you sure you want to quit?");
+    root.getChildren().add(options);
+    Scene scene = new Scene(root, 800, 600);  
+    
+    return scene;
   }
 
 }
