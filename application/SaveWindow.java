4f6613ae3313334872afc93ec8a2c171c002d755
ottobaier <obaier@wisc.edu>
2019-05-01 17:32:44 -0500

    save and quit works now

--- application/SaveWindow.java
+++ application/SaveWindow.java
@@ -1,6 +1,14 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SaveWindow implements Window{
@@ -16,7 +24,38 @@ public class SaveWindow implements Window{
 
   @Override
   public Scene getScene() {
-    // TODO Auto-generated method stub
-    return null;
+    VBox root = new VBox(20);
+    root.setPadding(new Insets(10, 25, 25, 25));
+    root.setSpacing(10);
+    Scene scene = new Scene(root, 500, 150);
+    // HEADER
+    Label saveHeader = new Label("Enter File Name");
+    saveHeader.setFont(Config.SIZE24);
+    root.getChildren().add(saveHeader);
+    // DESCRIPTION
+    Label loadDesc = new Label(".json");
+    loadDesc.setFont(Config.SIZE14);
+    // LOAD QUESTION FILE
+    HBox fileBox = new HBox(20);
+    Label fileLabel = new Label("Filename:");
+    fileLabel.setFont(Config.SIZE14);
+    fileBox.getChildren().add(fileLabel);
+    TextArea filename = new TextArea();
+    filename.setPrefWidth(250);
+    fileBox.getChildren().add(filename);
+    fileBox.setPrefHeight(10);
+    fileBox.getChildren().add(loadDesc);
+    root.getChildren().add(fileBox);
+    Button saveButton = new Button("Save and Quit");
+    // Attempting to load the entered filename
+    saveButton.setOnAction(new EventHandler<ActionEvent>() {
+        @Override public void handle(ActionEvent t) { 
+          Main.questionList.Save(filename.getText());
+          stage.close();
+        }
+    });
+    fileBox.getChildren().add(saveButton);
+    root.getChildren().add(new SwapScreen("Back",Main.windows[4],stage));
+    return scene;
   }
 }
