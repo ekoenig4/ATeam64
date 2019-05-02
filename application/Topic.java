9c7e9447d40ce31531243e7be9ed21e675ab9c3f
Angelique Stepanenkov <hexed.rabbit@gmail.com>
2019-05-02 09:50:36 -0500

    commented Topic

--- application/Topic.java
+++ application/Topic.java
@@ -1,24 +1,26 @@
-/**
- * Filename: Topic.java
- * Class: CS 400, Spring 2019 
- * Project: Final Team Project 
- * Due Date: May 2, 2019 
- * Authors: Alexandra Borukhovetskaya, Evan Koenig, Angelique Stepanenkov, Matthew Palmer, Otto Baier
- */
+//
+// Filename: Topic.java
+// Class: CS 400, Spring 2019
+// Project: Final Team Project
+// Due Date: May 2, 2019
+// Authors: Alexandra Borukhovetskaya, Evan Koenig, Angelique Stepanenkov, Matthew Palmer, Otto
+// Baier
+//
 package application;
 
 import java.util.ArrayList;
 
 /**
  * This class creates a topic with an ArrayList of questions associated with it.
- * @author
  *
  */
 public class Topic {
-  private String topicName;
-  private ArrayList<Question> questions;
+  private String topicName; // stores the topic's name
+  private ArrayList<Question> questions; // stores all the questions with this topic's name
 
-  /** Constructor to create a new topic and initialize it to an empty list
+  /**
+   * Constructor to create a new topic and initialize it to an empty list
+   * 
    * @param topicName Name for the new topic
    */
   public Topic(String topicName) {
@@ -27,28 +29,36 @@ public class Topic {
     Main.topics.add(topicName);
   }
 
-  /** Getter for the topic's name
+  /**
+   * Getter for the topic's name
+   * 
    * @return The name of the topic
    */
   public String getTopicName() {
     return topicName;
   }
 
-  /** Add a question to the ArrayList of questions associated to the topic
+  /**
+   * Add a question to the ArrayList of questions associated to the topic
+   * 
    * @param question The question to add to the topic
    */
   public void addQuestion(Question question) {
     questions.add(question);
   }
 
-  /** Getter for the number of questions for the topic
+  /**
+   * Getter for the number of questions for the topic
+   * 
    * @return number of questions
    */
   public int getNumQuestions() {
     return questions.size();
   }
 
-  /** Checks if a question is in the topic's list of questions
+  /**
+   * Checks if a question is in the topic's list of questions
+   * 
    * @param question Question to find
    * @return True if found, false otherwise
    */
@@ -56,7 +66,9 @@ public class Topic {
     return questions.contains(question);
   }
 
-  /** Gets all the questions for the topic
+  /**
+   * Gets all the questions for the topic
+   * 
    * @return An arrayList of questions
    */
   public ArrayList<Question> getQuestions() {
