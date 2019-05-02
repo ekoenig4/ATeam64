b0302f9e424054c9e1d47def32bee276db135459
Angelique Stepanenkov <hexed.rabbit@gmail.com>
2019-05-01 23:04:26 -0500

    Results is fully commented

--- application/Results.java
+++ application/Results.java
@@ -1,19 +1,38 @@
+//
+// Filename: AddQuestion.java
+// Class: CS 400, Spring 2019
+// Project: Final Team Project
+// Due Date: May 2, 2019
+// Authors: Alexandra Borukhovetskaya, Evan Koenig, Angelique Stepanenkov, Matthew Palmer, Otto
+// Baier
+//
+
 package application;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+/**
+ * This is the final window of the quiz. This displays the percentage correct from the quiz, the
+ * number of correct questions, and the total number of questions. The user then has the option to
+ * go back to the Home page where they can create a new quiz, add/load questions, save, or quit.
+ */
 public class Results implements Window {
   private Stage stage;
-  private int numCorrect;
-  private int numQuestions;
+  private int numCorrect; // the number of correct answers from the quiz
+  private int numQuestions; // the total number of questions from the quiz
 
+  /**
+   * Constructor that initializes the window's stage to the stage parameter passed int, and receives
+   * the number of correct answers and total number of questions from the quiz class.
+   * 
+   * @param stage
+   * @param numCorrect The user's number of correct answers
+   * @param numQuestions The total number of questions for the current quiz
+   */
   public Results(Stage stage, int numCorrect, int numQuestions) {
     this.stage = stage;
     this.numCorrect = numCorrect;
@@ -21,6 +40,13 @@ public class Results implements Window {
 
   }
 
+  /**
+   * The main display of the window. This displays the user's results from the most recent quiz they
+   * finished. The option to go back to the Home screen is available as a button at the bottom of
+   * the screen.
+   * 
+   * @see application.Window#getScene()
+   */
   @Override
   public Scene getScene() {
     // initialize window
@@ -33,18 +59,20 @@ public class Results implements Window {
     resultHeader.setFont(Config.SIZE24);
     root.getChildren().add(resultHeader);
     // create labels for % correct, num correct, and num questions
-    Label correctPerc = new Label("Percentage correct: " + ((double) numCorrect / (double) numQuestions * (100.0)) + "%");
+    Label correctPerc = new Label(
+        "Percentage correct: " + ((double) numCorrect / (double) numQuestions * (100.0)) + "%");
     Label numCorrectMsg = new Label("Number of Questions Correct: " + numCorrect);
     Label numQuestionMsg = new Label("Total Number of Questions in the Quiz: " + numQuestions);
+    // make all labels bold (except header)
     correctPerc.setFont(Config.BOLD18);
     numCorrectMsg.setFont(Config.BOLD18);
     numQuestionMsg.setFont(Config.BOLD18);
+    // add labels to be displayed on the screen
     root.getChildren().add(correctPerc);
     root.getChildren().add(numCorrectMsg);
     root.getChildren().add(numQuestionMsg);
+    // button to return to the home screen
     root.getChildren().add(new SwapScreen("Return to Home", Main.windows[0], stage));
-
     return scene;
   }
-
 }
