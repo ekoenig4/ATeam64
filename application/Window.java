bf2514b070be048071b2baa85be873e65afe9d9d
Angelique Stepanenkov <hexed.rabbit@gmail.com>
2019-05-02 09:48:54 -0500

    commented Window

--- application/Window.java
+++ application/Window.java
@@ -1,16 +1,18 @@
-/**
- * Filename: Window.java
- * Class: CS 400, Spring 2019 
- * Project: Final Team Project 
- * Due Date: May 2, 2019 
- * Authors: Alexandra Borukhovetskaya, Evan Koenig, Angelique Stepanenkov, Matthew Palmer, Otto Baier
- */
+//
+// Filename: Window.java
+// Class: CS 400, Spring 2019
+// Project: Final Team Project
+// Due Date: May 2, 2019
+// Authors: Alexandra Borukhovetskaya, Evan Koenig, Angelique Stepanenkov, Matthew Palmer, Otto
+// Baier
+//
 package application;
 
 import javafx.scene.Scene;
 
 /**
- * This makes window
+ * This is the window interface that all of the window classes implement. This holds the scene for
+ * all the windows and is used in the Main class to keep a list of all the windows together.
  *
  */
 public interface Window {
