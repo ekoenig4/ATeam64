7afc542e31e03d3a7ad3dc3d654f6237e4bfa881
ottobaier <obaier@wisc.edu>
2019-05-03 17:11:58 -0500

    added backgrounds to config

--- application/Config.java
+++ application/Config.java
@@ -7,6 +7,14 @@
  */
 package application;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -17,13 +25,17 @@ import javafx.scene.text.FontWeight;
  * @author Angelique Stepanenkov
  */
 public class Config {
-
-  /**
-   * Font sizes used in program
-   */
+  // Font sizes used in program
   public static final Font SIZE24 = Font.font(20); // font size of title headers
   public static final Font SIZE14 = Font.font(14); // font size of subheaders
   public static final Font BOLD18 = Font.font("Arial", FontWeight.BOLD, 18); // bold subheader
-  public static final Font TITLE = Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, FontPosture.ITALIC, 80);
+  public static final Font TITLE = Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, 
+      FontPosture.ITALIC, 80);
+  // Backgrounds
+  public static final Background GRADIENT  = new Background(new BackgroundFill(new LinearGradient
+      (0, 0, 0, 1, true, CycleMethod.NO_CYCLE, new Stop[] { new Stop(0, Color.LIGHTSKYBLUE), new 
+          Stop(1, Color.AQUAMARINE)}), CornerRadii.EMPTY, Insets.EMPTY));
+  public static final Background DISABLED_BUTTON = new Background(new BackgroundFill(Color.DARKGRAY, 
+      null, null));
 }
 
